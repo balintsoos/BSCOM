febc0ecf6636252c1ea6061bcd75e81cb2e69543
Balint Soos <balint.soos@edmdesigner.com>
2015-09-25 15:06:50 +0200

    transplantation to Angular.js

---
+++ src/js/legacy.js
@@ -0,0 +1,133 @@
+function AppViewModel () {
+	var self = this;
+
+	self.headerTitle = ko.observable("Balint Soos");
+	self.isSticky = ko.observable(false);
+	self.sections = ko.observableArray();
+	self.selectedItem = ko.observable({});
+
+	$(".nav-mobile").hide();
+	var showNav = false;
+
+	self.selectItem = function (item) {
+		self.selectedItem(item);
+		location.href = "#" + item.id;
+		setTimeout(self.toggleNav, 400);
+	};
+
+	self.goHome = function () {
+		location.href = "#home";
+		//
+	};
+
+	var wpHome = new Waypoint({
+		element: document.getElementById('home'),
+		handler: function () {
+			self.selectedItem({});
+		},
+		offset: '-50%',
+	});
+
+	var wpSticky = new Waypoint({
+		element: document.getElementById('aboutme'),
+		handler: function () {
+			if(self.isSticky()) {
+				self.isSticky(false);
+			} else {
+				self.isSticky(true);
+			}
+		},
+		offset: -1,
+	});
+
+	var secItem = function (id, title, header, icon, content) {
+		var obj 				= {};
+		obj.id 					= id;
+		obj.title 			= title;
+		obj.header 			= header;
+		obj.icon 				= icon;
+		obj.content 		= content;
+		obj.isSelected 	= ko.computed(function () {
+			return (self.selectedItem().id === id);
+		});
+		obj.wpTop = new Waypoint({
+			element: document.getElementById(id),
+			handler: function () {
+				self.setSelectedItem(id);
+			},
+			offset: '20%',
+		});
+		obj.wpBottom = new Waypoint({
+			element: document.getElementById(id),
+			handler: function () {
+				self.setSelectedItem(id);
+			},
+			offset: '-50%',
+		});
+		return obj;
+	};
+
+	self.setSelectedItem = function (id) {
+		self.sections().forEach(function (section) {
+			if (section.id === id) {
+				self.selectedItem(section);
+			}
+		});
+	};
+	
+	self.toggleNav = function () {
+		self.iconate();
+		$(".nav-mobile").slideToggle(300, "swing", function() {
+			showNav = !showNav;
+		});
+	}
+	var iconElement = document.getElementById('iconate');
+	self.iconate = function () {
+		var options = {
+			from: '',
+			to: '',
+			animation: 'tada',
+		}
+		if(showNav) {
+			options.from = 'fa-times';
+			options.to = 'fa-bars';
+		} else {
+			options.to = 'fa-times';
+			options.from = 'fa-bars';
+		}
+		iconate(iconElement, options);
+	}
+
+	self.init = function () {
+		self.sections([
+			secItem(
+				'aboutme',
+				'About Me',
+				'About Me',
+				'fa-user'
+			),
+			secItem(
+				'work',
+				'Work',
+				"I've worked with",
+				'fa-code'
+			),
+			secItem(
+				'projects',
+				'Projects',
+				'My recent projects',
+				'fa-archive'
+			),
+			secItem(
+				'contact',
+				'Contact',
+				'Contact with me',
+				'fa-comments'
+			)
+		]);
+	};
+	
+	self.init();
+}
+
+ko.applyBindings(new AppViewModel());
--- src/js/main.js
+++ src/js/main.js
@@ -1,89 +1,79 @@
+(function() {
+	var NavItem = (function() {
+		function NavItem (id, title, header, icon, content) {
+			this.id 				= id;
+			this.title 			= title;
+			this.header 		= header;
+			this.icon 			= icon;
+			this.content 		= content;
+			this.waypoints 	= {
+				wpTop: new Waypoint({
+					element: document.getElementById(id),
+					handler: function () {
+						setSelectedNavItem(id);
+					},
+					offset: '20%',
+				}),
+				wpBottom: new Waypoint({
+					element: document.getElementById(id),
+					handler: function () {
+						setSelectedNavItem(id);
+					},
+					offset: '-50%',
+				}),
+			};
+		}
+		
+		NavItem.prototype.isSelected = function() {
+			return this.id === selectedNavItem;
+		}
 
-function AppViewModel () {
-	var self = this;
+		return NavItem;
+	})();
 
-	self.headerTitle = ko.observable("Balint Soos");
-	self.isSticky = ko.observable(false);
-	self.sections = ko.observableArray();
-	self.selectedItem = ko.observable({});
+	var app = angular.module('bs', []);
+
+	app.controller('navItemsController', function() {
+		this.items = navItems;
+	});
+
+	app.controller('headerController', ['$scope', function($scope) {
+		$scope.isSticky = false;
+
+		var wpSticky = new Waypoint({
+			element: document.getElementById('aboutme'),
+			handler: function ($scope) {
+				$scope.isSticky = !$scope.isSticky;
+			},
+			offset: -1,
+		});
+	}]);
 
 	$(".nav-mobile").hide();
 	var showNav = false;
-
-	self.selectItem = function (item) {
-		self.selectedItem(item);
-		location.href = "#" + item.id;
-		setTimeout(self.toggleNav, 400);
-	};
-
-	self.goHome = function () {
-		location.href = "#home";
-		//
-	};
-
+	var selectedNavItem = "";
+	var iconElement = document.getElementById('iconate');
+	
 	var wpHome = new Waypoint({
 		element: document.getElementById('home'),
 		handler: function () {
-			self.selectedItem({});
+			setSelectedNavItem("");
 		},
 		offset: '-50%',
 	});
 
-	var wpSticky = new Waypoint({
-		element: document.getElementById('aboutme'),
-		handler: function () {
-			if(self.isSticky()) {
-				self.isSticky(false);
-			} else {
-				self.isSticky(true);
-			}
-		},
-		offset: -1,
-	});
+	var setSelectedNavItem = function(id) {
+		selectedNavItem = id;
+	}
 
-	var secItem  = function (id, title, header, icon, content) {
-		var obj 				= {};
-		obj.id 					= id;
-		obj.title 			= title;
-		obj.header 			= header;
-		obj.icon 				= icon;
-		obj.content 		= content;
-		obj.isSelected 	= ko.computed(function () {
-			return (self.selectedItem().id === id);
-		});
-		obj.wpTop = new Waypoint({
-			element: document.getElementById(id),
-			handler: function () {
-				self.setSelectedItem(id);
-			},
-			offset: '20%',
-		});
-		obj.wpBottom = new Waypoint({
-			element: document.getElementById(id),
-			handler: function () {
-				self.setSelectedItem(id);
-			},
-			offset: '-50%',
-		});
-		return obj;
-	};
-
-	self.setSelectedItem = function (id) {
-		self.sections().forEach(function (section) {
-			if (section.id === id) {
-				self.selectedItem(section);
-			}
-		});
-	};
-	
-	self.toggleNav = function () {
-		self.iconate();
+	var toggleNav = function() {
+		initIconate();
 		$(".nav-mobile").slideToggle(300, "swing", function() {
 			showNav = !showNav;
 		});
 	}
-	var iconElement = document.getElementById('iconate');
-	self.iconate = function () {
+
+	var initIconate = function () {
 		var options = {
 			from: '',
 			to: '',
@@ -99,36 +89,30 @@ function AppViewModel () {
 		iconate(iconElement, options);
 	}
 
-	self.init = function () {
-		self.sections([
-			secItem(
-				'aboutme',
-				'About Me',
-				'About Me',
-				'fa-user'
-			),
-			secItem(
-				'work',
-				'Work',
-				"I've worked with",
-				'fa-code'
-			),
-			secItem(
-				'projects',
-				'Projects',
-				'My recent projects',
-				'fa-archive'
-			),
-			secItem(
-				'contact',
-				'Contact',
-				'Contact with me',
-				'fa-comments'
-			)
-		]);
-	};
-	
-	self.init();
-}
-
-ko.applyBindings(new AppViewModel());
+	var navItems = [
+		new NavItem(
+			'aboutme',
+			'About Me',
+			'...but I do',
+			'fa-user'
+		),
+		new NavItem(
+			'work',
+			'Work',
+			"I've worked with",
+			'fa-code'
+		),
+		new NavItem(
+			'projects',
+			'Projects',
+			'My recent projects',
+			'fa-archive'
+		),
+		new NavItem(
+			'contact',
+			'Contact',
+			'Contact with me',
+			'fa-comments'
+		)
+	];
+})();
