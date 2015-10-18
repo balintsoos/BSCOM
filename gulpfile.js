fe373763f11a9cbb15b45c8495bd948f5ba5d5af
Balint Soos <soosbalint95@gmail.com>
2015-10-18 05:55:54 +0200

    css build w/ gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,50 @@
-var gulp        = require('gulp');
-var browserify  = require('gulp-browserify');
-var rename      = require('gulp-rename');
-var gutil       = require('gulp-util');
+var gulp          = require('gulp');
+var sourcemaps    = require('gulp-sourcemaps');
+var browserify    = require('gulp-browserify');
+var rename        = require('gulp-rename');
+var gutil         = require('gulp-util');
+var sass          = require('gulp-sass');
+var postcss       = require('gulp-postcss');
+var autoprefixer  = require('autoprefixer');
 
-gulp.task('build-JS', function () {
+// JS build
+gulp.task('build:JS', function () {
   gulp.src('./src/js/main.js')
     .pipe(browserify({
       insertGlobals : false,
-      debug : true,
     }))
     .pipe(rename('app.js'))
     .pipe(gulp.dest('./build/js'));
     
-  gutil.log(gutil.colors.magenta('Build ended succesfully'));
+  //gutil.log(gutil.colors.magenta('Build ended succesfully'));
 });
 
-gulp.task('build-CSS', function () {
-  gulp.src('./src/css/stlye.scss')
+// Sass and CSS build
+gulp.task('build:CSS', function () {
+  var processors = [
+    autoprefixer({browsers: ['last 2 versions']})
+  ];
+
+  gulp.src('./src/css/style.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss(processors))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./build/css'));
     
-    
-  gutil.log(gutil.colors.magenta('Build ended succesfully'));
+  //gutil.log(gutil.colors.magenta('Build ended succesfully'));
 });
 
-gulp.watch('./src/js/**/*.js', ['build-JS'])
-  .on('change', function (event) {
-    console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-  });
+// Watchers
+gulp.task('watch', function () {
+  gulp.watch('./src/js/**/*.js', ['build:JS'])
+    .on('change', function (event) {
+      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+  gulp.watch('./src/css/**/*.scss', ['build:CSS'])
+    .on('change', function (event) {
+      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+});
 
-gulp.task('default', ['build-JS']);
+gulp.task('default', ['build:JS', 'build:CSS', 'watch']);
